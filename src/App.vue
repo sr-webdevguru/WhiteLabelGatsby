a4a401bda99c163fd11ce9507ebbe5e622ce8d85
root <crystalmanner130@outlook.com>
2012-05-03 16:09:41 -0700

    App.vue added

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <div class="cm-container">
+      <div class="cm-logo-wrapper">
+        <img alt="Carb Manager" src="./assets/cm-logo.svg" class="cm-logo" />
+      </div>
+      <h2>Carb Manager Dev Assignment</h2>
+      <p>See the README file for assignment requirements.</p>
+
+      <ul>
+        <li v-for="recipe in recipes" :key="recipe" class="premium-recipe">
+          <PremiumRecipeCard :id="recipe" />
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import PremiumRecipeCard from "./components/PremiumRecipeCard.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    PremiumRecipeCard
+  },
+
+  data: () => ({
+    recipes: ["Premium", "recipes", "list", "goes", "here"]
+  })
+};
+</script>
+
+<style>
+#app {
+  font-family: "proxima-nova", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<style scoped>
+.cm-logo-wrapper {
+  margin-bottom: 30px;
+}
+
+.cm-logo {
+  max-width: 150px;
+  height: auto;
+}
+
+.cm-container {
+  max-width: 960px;
+  margin: auto;
+}
+
+/** Remove these styles when done */
+.premium-recipe {
+  margin-top: 24px;
+  border: 2px dashed red;
+  padding: 16px;
+  list-style: none;
+}
+</style>
